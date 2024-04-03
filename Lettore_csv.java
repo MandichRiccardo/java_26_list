2ff3c1a44a384729d2519553ca1bccf60e0e3668
MandichRiccardo <ricky.mandich@gmail.com>
2024-04-03 15:04:14 +0200

    aggiornamento 2024 04 03 15:00

---
+++ Lettore_csv.java
@@ -0,0 +1,72 @@
+public class Lettore_csv {
+    public String[] tabel;
+    public void getFile(String file) throws java.io.FileNotFoundException {
+        java.io.BufferedReader reader = null;
+        String line;
+        tabel = new String[0];
+        try{
+            reader = new java.io.BufferedReader(new java.io.FileReader(file));
+            while((line = reader.readLine()) != null){
+                aggiungiRiga(line);
+            }
+        }
+        catch (java.io.FileNotFoundException e){
+            throw new java.io.FileNotFoundException();
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        finally {
+            try {
+                reader.close();
+            } catch (java.io.IOException | NullPointerException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void orderAndCompact(){
+        boolean scambio = true;
+        while (scambio){
+            scambio = false;
+            for(int i=0;i<tabel.length-1;i++){
+                if(tabel[i].compareTo(tabel[i+1])>0){
+                    scambio = true;
+                    String temp = tabel[i];
+                    tabel[i] = tabel[i+1];
+                    tabel[i+1] = temp;
+                }
+            }
+        }
+        for(int i=1;i<tabel.length;i++){
+            if(tabel[i].compareTo(tabel[i-1]) == 0){
+                delete(i--);
+            }
+        }
+    }
+
+    public void delete(int h){
+        String[] newTabel = new String[tabel.length-1];
+        for(int i=0, j=0;j<newTabel.length;j++, i++){
+            if(i==h) i++;
+            newTabel[j] = tabel[i];
+        }
+        tabel = newTabel;
+    }
+
+    public void aggiungiRiga(String row){
+        String[] newTabel;
+        newTabel = new String[tabel.length+1];
+        System.arraycopy(tabel, 0, newTabel, 0, tabel.length);
+        newTabel[tabel.length] = row;
+        tabel = newTabel;
+    }
+
+
+    public void outElencoNomiFile(){
+        System.out.println("file csv:");
+        for(int i=0;i<tabel.length;i++){
+            System.out.println(i + ")\t" + tabel[i]);
+        }
+    }
+}
---
+++ Scrittore_csv.java
@@ -0,0 +1,112 @@
+public class Scrittore_csv {
+    java.io.FileWriter writer;
+
+    public Scrittore_csv(Lettore_csv reader, String file) {
+        try {
+            writer = new java.io.FileWriter("csv\\elencoFileDati");
+            reader.aggiungiRiga(file);
+            reader.orderAndCompact();
+
+            String info = "";
+            for (int i = 0; i < reader.tabel.length; i++) {
+                info = info.concat(reader.tabel[i] + "\n");
+            }
+            writer.write(info);
+        } catch (java.io.IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public Scrittore_csv(String file){
+        try {
+            writer = new java.io.FileWriter(file);
+
+            String info = "";
+            info += "nome";
+            info += "," + "iniziativa";
+            info += puntiFeritaToCsv();
+            info += "," + "classeArmatura";
+            info += "," + "competenza";
+            info += "," + "puntiEsperienza";
+            info += "," + "livello";
+            info += "," + "dannoIniziale";
+            info += "," + "amico";
+            info += "," + "morto";
+            info += forzaToCsv();
+            info += destrezzaToCsv();
+            info += costituzioneToCsv();
+            info += intelligenzaToCsv();
+            info += saggezzaToCsv();
+            info += carismaToCsv();
+
+            info += "," + "ispirazione";
+            info += "," + "bonusSalvezza";
+            info += tiriControMorteToCsv();
+            writer.write(info + "\n");
+        } catch (java.io.IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public String puntiFeritaToCsv(){
+        String info = "";
+        info += ",punti ferita attuali";
+        info += ",punti ferita totali";
+        return info;
+    }
+    public String caratteristicaToCsv(String caratteristica){
+        String info = "";
+        info += ",punteggio " + caratteristica;
+        info += ",bonus salvezza " + caratteristica;
+        return info;
+    }
+    public String forzaToCsv(){
+        return caratteristicaToCsv("forza");
+    }
+    public String destrezzaToCsv(){
+        return caratteristicaToCsv("destrezza");
+    }
+    public String costituzioneToCsv(){
+        return caratteristicaToCsv("costituzione");
+    }
+    public String intelligenzaToCsv(){
+        return caratteristicaToCsv("intelligenza");
+    }
+    public String saggezzaToCsv(){
+        return caratteristicaToCsv("saggezza");
+    }
+    public String carismaToCsv(){
+        return caratteristicaToCsv("carisma");
+    }
+    public String tiriControMorteToCsv(){
+        String info = "";
+        for(int i=0;i<2;i++){
+            for(int j=0;j<3;j++){
+                info = info.concat(tiroControMorteToCsv(i, j));
+            }
+        }
+        return info;
+    }
+    public String tiroControMorteToCsv(int i, int j){
+        return "," + (j+1) + ((i==0) ? " successo" : " fallimento") + " tiri contro morte";
+    }
+    public void addCsv(String stringa) {
+        try {
+            writer.append(stringa);
+        } catch (java.io.IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public void addFileName(String file){
+        try {
+            writer.write(file);
+        }catch (java.io.IOException e){
+            e.printStackTrace();
+        }
+    }
+    public void close(){
+        try {
+            writer.close();
+        } catch (java.io.IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
